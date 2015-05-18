a4549fafb43ef66bb71942fd7d5ad3ec6ed0813e
Zackkk <zhexie@usc.edu>
2015-05-17 19:20:22 -0700

    indentation updated

--- Assignment1/tweet_sentiment.py
+++ Assignment1/tweet_sentiment.py
@@ -1,29 +1,42 @@
 """
-Compute the sentiment of each tweet based on the sentiment scores (AFINN-111.txt) of the terms in the tweet. 
-The sentiment of a tweet is equivalent to the sum of the sentiment scores for each term in the tweet.
+input: sentiment computation file, tweet file
+output: sentiment score for each tweet
 
-The file AFINN-111.txt contains a list of pre-computed sentiment scores. 
-Each line in the file contains a word or phrase followed by a sentiment score. 
-Each word or phrase that is found in a tweet but not found in AFINN-111.txt should be given a sentiment score of 0. 
-See the file AFINN-README.txt for more information.
+Explanation:
+    Compute the sentiment of each tweet based on the sentiment scores (AFINN-111.txt, tab-delimited) of the terms in the tweet. 
+    The sentiment of a tweet is equivalent to the sum of the sentiment scores for each term in the tweet.
 
-$ python tweet_sentiment.py AFINN-111.txt output.txt   # output.txt is the input txt file
+    The file AFINN-111.txt contains a list of pre-computed sentiment scores. 
+    Each line in the file contains a word or phrase followed by a sentiment score. 
+    Each word or phrase that is found in a tweet but not found in AFINN-111.txt should be given a sentiment score of 0. 
+    See the file AFINN-README.txt for more information.
+
+    What information each tweet contains: https://dev.twitter.com/overview/api/tweets
+
+    $ python tweet_sentiment.py AFINN-111.txt output.txt   # output.txt is the input txt file
 """
 
 import sys
+import json
 
-def hw():
-    print ('Hello, world!')
+sentiment_dictionary = {}
+
+def parse_sentiment_file(sf):
+    for line in sf:
+        term, score = line.split("\t")
+        sentiment_dictionary[term] = int(score)
 
 def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
-    sent_file = open(sys.argv[1])
+    sentiment_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    parse_sentiment_file(sentiment_file)
+    
+
+    print sentiment_dictionary.items()
 
 if __name__ == '__main__':
     main()
--- Assignment1/twitter_stream.py
+++ Assignment1/twitter_stream.py
@@ -1,11 +1,12 @@
 """
-Output twitter stream of "https://stream.twitter.com/1/statuses/sample.json"
+Input: Url "https://stream.twitter.com/1/statuses/sample.json"
+Output its twitter stream 
 
-Need: 
-  oauth2 library: https://pypi.python.org/pypi/oauth2/
-  Twitter credentials: https://apps.twitter.com/
-
-$ python twitterstream.py > output.txt
+Explanation:
+    Dependencies:
+        oauth2 library: https://pypi.python.org/pypi/oauth2/
+        Twitter credentials: https://apps.twitter.com/
+    $ python twitterstream.py > output.txt
 """
 
 import oauth2 as oauth
@@ -33,40 +34,40 @@ Construct, sign, and open a twitter request
 using the hard-coded credentials above.
 """
 def twitter_request(url, method, parameters):
-  request = oauth.Request.from_consumer_and_token(oauth_consumer,
-                                             token = oauth_token,
-                                             http_method = http_method,
-                                             http_url = url, 
-                                             parameters = parameters)
-  request.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
-  headers = request.to_header()
+    request = oauth.Request.from_consumer_and_token(oauth_consumer,
+                                            token = oauth_token,
+                                            http_method = http_method,
+                                            http_url = url, 
+                                            parameters = parameters)
+    request.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+    headers = request.to_header()
 
-  if http_method == "POST":
-    encoded_post_data = request.to_postdata()
-  else:
-    encoded_post_data = None
-    url = request.to_url()
+    if http_method == "POST":
+        encoded_post_data = request.to_postdata()
+    else:
+        encoded_post_data = None
+        url = request.to_url()
 
-  opener = urllib.OpenerDirector()
-  opener.add_handler(http_handler)
-  opener.add_handler(https_handler)
+    opener = urllib.OpenerDirector()
+    opener.add_handler(http_handler)
+    opener.add_handler(https_handler)
 
-  response = opener.open(url, encoded_post_data)
-  return response
+    response = opener.open(url, encoded_post_data)
+    return response
 
 """
 Module starts here
 """
 def fetch_samples():
 
-  # tweet search API: https://dev.twitter.com/rest/reference/get/search/tweets
-  # e.g.:url = "https://api.twitter.com/1.1/search/tweets.json?q=microsoft"
+    # tweet search API: https://dev.twitter.com/rest/reference/get/search/tweets
+    # e.g.:url = "https://api.twitter.com/1.1/search/tweets.json?q=microsoft"
 
-  url = "https://stream.twitter.com/1/statuses/sample.json"
-  parameters = []
-  response = twitter_request(url, "GET", parameters)
-  for line in response:
-    print line
+    url = "https://stream.twitter.com/1/statuses/sample.json"
+    parameters = []
+    response = twitter_request(url, "GET", parameters)
+    for line in response:
+        print line
 
-if __name__ == '__main__':
-  fetch_samples()
+    if __name__ == '__main__':
+        fetch_samples()
